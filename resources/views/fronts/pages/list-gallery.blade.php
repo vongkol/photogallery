47f207a32c4d2e54e29b967934afca27dbab5033
vichey <vichey@vdoo.biz>
2017-12-29 10:43:58 +0700

    style

--- resources/views/fronts/pages/list-gallery.blade.php
+++ resources/views/fronts/pages/list-gallery.blade.php
@@ -5,24 +5,26 @@
 	<div class="col-md-12 col-sm-12">
 		<h4>Gallery</h4> <hr>
     </div>
-    <div class="row">
         @foreach($gallerys as $gal)
-            <div class="col-md-4 col-sm-4 vdoo-width">
-                <div class="content-page">
-                    <div class="col-md-6">
-                        <img src="{{asset('uploads/gallerys/'.$gal->category_id.'/'.$gal->gallery)}}" alt="gallery" height="100%" class="img-thumbnail">
-                    </div>
-                    <div class="col-md-6 col-g vdoo-width">
-                        {{$gal->description}}
-                    </div>
+            <div class="col-md-4 col-sm-4">
+                <div class="col-md-6 vdoo-width">
+                    <img src="{{asset('uploads/gallerys/'.$gal->category_id.'/'.$gal->gallery)}}" alt="gallery" class="max-height img-thumbnail">
+                </div>
+                <div class="col-md-6 col-g">
+                    {{$gal->description}}
                 </div>
             </div>
         @endforeach
-    </div>
     <div class="col-md-12 col-sm-12"><br><br>
         <nav class="text-center"> 
             {{$gallerys->links()}}
         </nav>
     </div>
 </div>
+<style>
+    img {height: auto;
+max-width: 100%;
+vertical-align: middle;
+border: 0;}
+</style>
 @endsection
